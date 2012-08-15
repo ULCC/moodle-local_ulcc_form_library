d157b1bfcaf79d6d1b45ae03a214652b15471503
Matt Gibson <matt@inaura.net>
2012-08-15 12:31:57 +0100

    Fixed the access check so that it will log in properly to the course and coursemodule if they are available.

--- db/accesscheck.php
+++ db/accesscheck.php
@@ -1,7 +1,21 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Perfrorms permissions checks against the user to see what they are allowed to
+ * Performs permissions checks against the user to see what they are allowed to
  * do.
  *
  * @copyright &copy; 2012 University of London Computer Centre
@@ -11,24 +25,31 @@
  * @version 1.0
  */
 
-global  $CFG, $USER, $PAGE;
+global  $CFG, $DB, $USER, $PAGE;
 
-//the user must be logged in
-require_login(0);
+$context_id = optional_param('context_id', 0, PARAM_INT);
+$context = false;
+if ($context_id) {
+    // We need to get the current context so we can if the user has the capability to use the forms library.
+    $context = context::instance_by_id($context_id, IGNORE_MISSING);
+}
 
-//we need to get the current context so we can if the user has the capababilty to use the forms library
-$context_id =   optional_param('context_id',0,PARAM_INT);
+if ($context && $context->contextlevel == CONTEXT_MODULE) {
+    $coursemodule = $DB->get_record('course_modules', array('id' => $context->instanceid));
+    $course = $DB->get_record('course', array('id' => $coursemodule->course));
+    require_login($course, false, $coursemodule);
+} else {
+    // The user must be logged in.
+    require_login(0);
+    $context = context_system::instance();
+}
 
-$context =   (!empty($context_id)) ? context::instance_by_id($context_id, IGNORE_MISSING) : context_system::instance();
-
-//if there is no user context then throw an error
+// If there is no user context then throw an error.
 if (empty($context)) {
     print_error("mustspecifycontext", 'local_ulcc_form_library');
 }
 
-//make sure that the user has the ability to manipulate forms if not throw an error
+// Make sure that the user has the ability to manipulate forms if not throw an error.
 if (!has_capability('local/ulcc_form_library:formadmin', $context) ) {
     print_error('not_form_admin', 'local_ulcc_form_library');
 }
-//this needs to be done but it is causing errors so I am commenting out for meanwhile ND
-$PAGE->set_context($context);
