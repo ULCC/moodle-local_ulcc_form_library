c5ee5f153ee196550f55b3d3ccf1dfdedbd305be
nigel daley <user@server>
2012-08-16 13:00:00 +0100

    Added grade plugin implemented only 1 instance of comment editor allowed

--- plugin/form_elements/form_element_plugin_grade.php
+++ plugin/form_elements/form_element_plugin_grade.php
@@ -2,19 +2,18 @@
 
 require_once($CFG->dirroot.'/local/ulcc_form_library/classes/form_element_plugin.class.php');
 
-class form_element_plugin_comment_editor extends form_element_plugin {
-
+class form_element_plugin_grade extends form_element_plugin {
+	
 	public $tablename;
 	public $data_entry_tablename;
-	public $minimumlength;		//defined by the form creator to validate user input
-	public $maximumlength;		//defined by the form creator to validate user input
-	
-	    /**
+
+	 /**
      * Constructor
      */
     function __construct() {
-    	$this->tablename = "ulcc_form_plg_cedt";
-    	$this->data_entry_tablename = "ulcc_form_plg_cedt_ent";
+    	
+    	$this->tablename = "ulcc_form_plg_grade";
+    	$this->data_entry_tablename = "ulcc_form_plg_grade_ent";
     	
     	parent::__construct();
     }
@@ -22,7 +21,7 @@ class form_element_plugin_comment_editor extends form_element_plugin {
 	
 	/**
      * TODO comment this
-     *
+     * called when user form is submitted
      */
     public function load($formfield_id) {
 		$formfield		=	$this->dbc->get_form_field_data($formfield_id);
@@ -34,17 +33,19 @@ class form_element_plugin_comment_editor extends form_element_plugin {
 			$plugin		=	$this->dbc->get_form_element_plugin($formfield->formelement_id);
 						
 			$this->formelement_id	=	$formfield->formelement_id;
-			
+
 			//get the form element record for the formfield
 			$pluginrecord	=	$this->dbc->get_form_element_by_formfield($this->tablename,$formfield->id);
 			
 			if (!empty($pluginrecord)) {
 				$this->label			=	$formfield->label;
 				$this->description		=	$formfield->description;
-				$this->required				=	$formfield->required;
-				$this->maximumlength	=	$pluginrecord->maximumlength;
-				$this->minimumlength	=	$pluginrecord->minimumlength;
+                $this->required				=	$formfield->required;
+                $this->gradetablename				=	$pluginrecord->tablename;
+                $this->gradetype				=	$pluginrecord->gradetype;
+                $this->gradescale				=	$pluginrecord->gradescale;
 				$this->position			=	$formfield->position;
+                $this->audit_type       =   $this->audit_type();
 				return true;	
 			}
 		}
@@ -53,7 +54,7 @@ class form_element_plugin_comment_editor extends form_element_plugin {
 
 	
 	/**
-     *
+     * create tables for this plugin
      */
     public function install() {
         global $CFG, $DB;
@@ -69,15 +70,20 @@ class form_element_plugin_comment_editor extends form_element_plugin {
         $table_form = new $this->xmldb_field('formfield_id');
         $table_form->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
         $table->addField($table_form);
-        
-        $table_minlength = new $this->xmldb_field('minimumlength');
-        $table_minlength->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
-        $table->addField($table_minlength);
-        
-        $table_maxlength = new $this->xmldb_field('maximumlength');
-        $table_maxlength->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
-        $table->addField($table_maxlength);
-        
+
+        $table_form = new $this->xmldb_field('gradetype');
+        $table_form->$set_attributes(XMLDB_TYPE_INTEGER, 1, XMLDB_UNSIGNED, null);
+        $table->addField($table_form);
+
+        $table_form = new $this->xmldb_field('gradescale');
+        $table_form->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, null);
+        $table->addField($table_form);
+
+        $table_form = new $this->xmldb_field('tablename');
+        $table_form->$set_attributes(XMLDB_TYPE_CHAR, 255,null);
+        $table->addField($table_form);
+
+
         $table_timemodified = new $this->xmldb_field('timemodified');
         $table_timemodified->$set_attributes(XMLDB_TYPE_INTEGER, 10, XMLDB_UNSIGNED, XMLDB_NOTNULL);
         $table->addField($table_timemodified);
@@ -90,15 +96,14 @@ class form_element_plugin_comment_editor extends form_element_plugin {
         $table_key->$set_attributes(XMLDB_KEY_PRIMARY, array('id'));
         $table->addKey($table_key);
 
-        $table_key = new $this->xmldb_key('commenteditorplugin_unique_formfield');
-        $table_key->$set_attributes(XMLDB_KEY_FOREIGN_UNIQUE, array('formfield_id'),'ulcc_form_lib_form_field','id');
+        $table_key = new $this->xmldb_key('grade_unique_formfield');
+        $table_key->$set_attributes(XMLDB_KEY_FOREIGN_UNIQUE, array('formfield_id'),'local_ulcc_form_library_form_field','id');
         $table->addKey($table_key);
-        
-        
 
         if(!$this->dbman->table_exists($table)) {
             $this->dbman->create_table($table);
         }
+        
         
 	    // create the new table to store responses to fields
         $table = new $this->xmldb_table( $this->data_entry_tablename );
@@ -109,7 +114,7 @@ class form_element_plugin_comment_editor extends form_element_plugin {
         $table->addField($table_id);
         
         $table_title = new $this->xmldb_field('value');
-        $table_title->$set_attributes(XMLDB_TYPE_TEXT);
+        $table_title->$set_attributes(XMLDB_TYPE_CHAR, 255, null, null);
         $table->addField($table_title);
 
         $table_form = new $this->xmldb_field('entry_id');
@@ -158,21 +163,24 @@ class form_element_plugin_comment_editor extends form_element_plugin {
      *
      */
     public function audit_type() {
-        return get_string('form_element_plugin_comment_editor_type','local_ulcc_form_library');
+        return get_string('form_element_plugin_grade_type','local_ulcc_form_library');
     }
     
     /**
     * function used to return the language strings for the plugin
     */
     function language_strings(&$string) {
-        $string['form_element_plugin_comment_editor'] 		= 'Grade comment editor';
-        $string['form_element_plugin_comment_editor_type'] = 'Grade comment editor';
-        $string['form_element_plugin_comment_editor_description'] = 'A grade comment editor';
-        $string['form_element_plugin_comment_editor_minimumlength'] = 'Minimum Length';
-        $string['form_element_plugin_comment_editor_maximumlength'] = 'Maximum Length';
-        $string['form_element_plugin_comment_editor_maxlengthrange'] = 'The maximum length field must have a value between 0 and 255';
-        $string['form_element_plugin_comment_editor_maxlessthanmin'] = 'The maximum length field must have a greater value than the minimum length';
-        
+        $string['form_element_plugin_grade'] 		= 'Grade selector';
+        $string['form_element_plugin_grade_type']    = 'Grade selector';
+        $string['form_element_plugin_grade_description'] = 'A grade selector';
+        $string['form_element_plugin_grade_dynamicdesc'] =   'The dynamic checkbox below defines whether the grade
+        produced in Module grade selector will be chosen now or will be chosen at run time using the data taken from the given
+        database. If you choose to make the selector dynamic then choose the module that you are working with.';
+        $string['form_element_plugin_grade_gradetype'] =   'Dynamic grade selector';
+        $string['form_element_plugin_grade_module'] =   'Module: ';
+        $string['form_element_plugin_grade_gradescale'] =   'Grade scale';
+
+
         return $string;
     }
 
@@ -180,37 +188,59 @@ class form_element_plugin_comment_editor extends form_element_plugin {
      * Delete a form element
      */
     public function delete_form_element($formfield_id, $tablename=null, $extraparams=null) {
-    	return parent::delete_form_element($formfield_id, $this->tablename);
+		$formfield		=	$this->dbc->get_form_field_data($formfield_id);
+        $extraparams = array(
+            'audit_type' => $this->audit_type(),
+            'label' => $formfield->label,
+            'description' => $formfield->description,
+            'id' => $formfield_id
+        );
+    	return parent::delete_form_element($formfield_id, $this->tablename, $extraparams);
     }
     
     /**
-    * this function returns the mform elements that will be added to a form form
+    * this function returns the mform elements taht will be added to a form form
 	*
     */
     public	function entry_form( &$mform ) {
+        global  $DB;
     	
-    	//create the fieldname
     	$fieldname	=	"{$this->formfield_id}_field";
-    	
     	if (!empty($this->description)) {
     		$mform->addElement('static', "{$fieldname}_desc", $this->label, strip_tags(html_entity_decode($this->description),FORM_STRIP_TAGS_DESCRIPTION));
     		$this->label = '';
-    	} 
-    	//text field for element label
-   		$mform->addElement(
-	           'htmleditor',
-	            $fieldname,
-	            "$this->label",
-	            array('class' => 'form_input', 'canUseHtmlEditor'=>'detect', 'rows'=> '20', 'cols'=>'65')
-	    );
-    	
+    	}
 
-        if (!empty($this->minimumlength)) $mform->addRule($fieldname, null, 'minlength', $this->minimumlength, 'client');
-        if (!empty($this->maximumlength)) $mform->addRule($fieldname, null, 'maxlength', $this->maximumlength, 'client');
+        if (empty($this->gradetype))    {
+
+            $scale  =   $DB->get_record('scale',array('id'=>$this->gradescale));
+
+            $grademenu   =  make_menu_from_list($scale->scale);
+
+        } else{
+
+            //the user has selected the dynamic grade type for the grade form element
+            //selecting this comes with the proviso that the user must supply the param
+            //graderecordid in query string of the page calling the form. This param will
+            //be used in conjunction with the tablename provided to get a record that
+            //holds a field called grade which will be used to retrieve the grade scale
+
+            $graderecordid     =    optional_param('graderecordid',0,PARAM_RAW);
+
+            $tablerecord        =   $DB->get_record($this->gradetablename,array('id'=>$graderecordid));
+
+            //if a record with a grade has been found then populate gradesmenu with this
+            $grademenu = (!empty($tablerecord)) ? make_grades_menu($tablerecord->grade) : array();
+        }
+
+        $mform->addElement('select',
+                            $fieldname,
+                            "$this->label",
+                            $grademenu);
+        
         if (!empty($this->required)) $mform->addRule($fieldname, null, 'required', null, 'client');
-        $mform->setType($fieldname, PARAM_RAW);
-
-    }
+	 }
+	 
 	/**
 	* handle user input
 	**/
@@ -223,11 +253,15 @@ class form_element_plugin_comment_editor extends form_element_plugin {
 		return $this->entry_process_data($formfield_id,$entry_id,$data);
 	 }
 
-    /*
-    * should not be able to add a status selector if there is already one one the form
-    */
-    public function can_add( $form_id ){
+    /**
+     * Determines whether a user can add a new instance of the mod grade plugin to the form
+     *
+     * @param int $form_id the id of the form that will be checked to see if it has the element
+     * @return bool
+     */
+    function can_add($form_id)  {
         return !$this->dbc->element_type_exists( $form_id, $this->tablename );
     }
+	 
+	 
 }
-
---
+++ plugin/form_elements/form_element_plugin_grade_mform.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once($CFG->dirroot.'/local/ulcc_form_library/classes/form_element_plugin_mform.class.php');
+
+class form_element_plugin_grade_mform  extends form_element_plugin_mform {
+	
+	  	
+	
+	protected function specific_definition($mform) {
+
+        global  $DB;
+
+        $mform->addElement('html','<div>'.get_string('form_element_plugin_grade_dynamicdesc', 'local_ulcc_form_library').'</div>');
+
+        $mform->addElement('advcheckbox', 'gradetype', get_string('form_element_plugin_grade_gradetype', 'local_ulcc_form_library'), '', array('group' => 1), array(0, 1));
+
+        $modules     =   $DB->get_records('modules',array('visible'=>1));
+
+        $options    =   array();
+
+        foreach($modules as $m)   {
+            $options[$m->name]    =   $m->name;
+        }
+
+        $mform->addElement('select', 'tablename', get_string('form_element_plugin_grade_module', 'local_ulcc_form_library'), $options);
+
+        $mform->setDefault('tablename', $this->moodlepluginname);
+
+        $mform->disabledIf('tablename','gradetype','unchecked');
+
+        $scales     =   $DB->get_records('scale');
+        $options    =   array();
+
+        foreach($scales as $s)   {
+            $options[$s->id]    =   $s->name;
+        }
+
+        $mform->addElement('select', 'gradescale', get_string('form_element_plugin_grade_gradescale', 'local_ulcc_form_library'), $options);
+
+        $mform->disabledIf('gradescale','gradetype','checked');
+
+	}
+	
+	protected function specific_validation($data) {
+ 	
+	 	$data = (object) $data;
+
+	 	return $this->errors;
+	 }
+	 
+	 protected function specific_process_data($data) {
+	  	
+	 	$plgrec = (!empty($data->formfield_id)) ? $this->dbc->get_form_element_record("ulcc_form_plg_grade",$data->formfield_id) : false;
+	 	
+	 	if (empty($plgrec)) {
+	 		return $this->dbc->create_form_element_record("ulcc_form_plg_grade",$data);
+	 	} else {
+	 		//get the old record from the elements plugins table 
+	 		$oldrecord				=	$this->dbc->get_form_element_by_formfield("ulcc_form_plg_grade",$data->formfield_id);
+	
+	 		//create a new object to hold the updated data
+	 		$pluginrecord 					=	new stdClass();
+	 		$pluginrecord->id				=	$oldrecord->id;
+
+	 		//update the plugin with the new data
+	 		return $this->dbc->update_form_element_record("ulcc_form_plg_grade",$pluginrecord);
+	 	}
+	 }
+	 
+	 function definition_after_data() {
+	 	
+	 }
+
+
+
+}
