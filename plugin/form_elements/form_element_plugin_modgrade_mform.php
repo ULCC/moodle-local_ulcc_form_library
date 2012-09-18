23ddd8cfcd0bbfc0d450b607a836b7308db29ab5
aleksandra <a.ferenz@ulcc.ac.uk>
2012-09-18 09:34:33 +0100

    [FIXED #35706715] Added help button next to required checkbox for modgrade plugin.

--- plugin/form_elements/form_element_plugin_modgrade_mform.php
+++ plugin/form_elements/form_element_plugin_modgrade_mform.php
@@ -24,6 +24,128 @@ require_once($CFG->dirroot.'/local/ulcc_form_library/classes/form_element_plugin
  */
 class form_element_plugin_modgrade_mform extends form_element_plugin_mform {
 
+
+    function definition() {
+        global $USER, $CFG;
+
+        //get the plugin type by getting the plugin name
+        $currentplugin	=	$this->dbc->get_form_element_plugin($this->formelement_id);
+
+        $form           =   $this->dbc->get_form_by_id($this->form_id);
+
+        $mform =& $this->_form;
+        $fieldsettitle	=	get_string("addfield",'local_ulcc_form_library');
+
+        //define the elements that should be present on all plugin element forms
+
+        //create a fieldset to hold the form
+        $mform->addElement('html', '<fieldset id="formfieldset" class="clearfix formfieldset">');
+        $mform->addElement('html', '<legend class="ftoggler">'.$form->name.'</legend>');
+
+        //the id of the form that the element will be in
+        $mform->addElement('hidden', 'id');
+        $mform->setType('id', PARAM_INT);
+
+        $mform->addElement('static', 'plugintypestatic',get_string('plugintype','local_ulcc_form_library'),get_string($currentplugin->name.'_type','local_ulcc_form_library'));
+
+
+        //button to state whether the element is required
+        $mform->addElement('checkbox',
+            'required',
+            get_string('required', 'local_ulcc_form_library')
+        );
+
+        $mform->addHelpButton('required', 'required', 'local_ulcc_form_library');
+
+        //button to state whether the element is required
+        if ($this->moodlepluginname != 'coursework'){
+            $mform->addElement('checkbox',
+                'summary',
+                get_string('addtosummary', 'local_ulcc_form_library')
+            );
+        }
+        //the id of the form that the element will be in
+        $mform->addElement('hidden', 'form_id');
+        $mform->setType('form_id', PARAM_INT);
+        $mform->setDefault('form_id', $this->form_id);
+
+        //the id of the plugin in use
+        $mform->addElement('hidden', 'formelement_id');
+        $mform->setType('formelement_id', PARAM_INT);
+        $mform->setDefault('formelement_id', $this->formelement_id);
+
+        //the id of the form element creator
+        $mform->addElement('hidden', 'creator_id');
+        $mform->setType('creator_id', PARAM_INT);
+        $mform->setDefault('creator_id', $this->creator_id);
+
+        //the id of the course that the element is being created in
+        $mform->addElement('hidden', 'course_id');
+        $mform->setType('course_id', PARAM_INT);
+        $mform->setDefault('course_id', $this->course_id);
+
+
+        //the id of the formfield this is only used in edit instances
+        $mform->addElement('hidden', 'formfield_id');
+        $mform->setType('formfield_id', PARAM_INT);
+        $mform->setDefault('formfield_id', $this->formfield_id);
+
+        //the moodle plugin type of the form
+        $mform->addElement('hidden', 'moodleplugintype');
+        $mform->setType('moodleplugintype', PARAM_INT);
+        $mform->setDefault('moodleplugintype', $this->moodleplugintype);
+
+        //the moodle plugin type of the form
+        $mform->addElement('hidden', 'moodlepluginname');
+        $mform->setType('moodlepluginname', PARAM_RAW);
+        $mform->setDefault('moodlepluginname', $this->moodlepluginname);
+
+        //the moodle context_id
+        $mform->addElement('hidden', 'context_id');
+        $mform->setType('context_id', PARAM_RAW);
+        $mform->setDefault('context_id', $this->context_id);
+
+
+        //the id of the form element creator
+        $mform->addElement('hidden', 'position');
+        $mform->setType('position', PARAM_INT);
+        //set the field position of the field
+        $mform->setDefault('position', $this->dbc->get_new_form_field_position($this->form_id));
+
+
+
+        //text field for element label
+        $mform->addElement(
+            'text',
+            'label',
+            get_string('label', 'local_ulcc_form_library'),
+            array('class' => 'form_input')
+        );
+
+        $mform->addRule('label', null, 'maxlength', 255, 'client',array('size'=>'10'));
+        $mform->addRule('label', null, 'required', null, 'client');
+        $mform->setType('label', PARAM_RAW);
+
+
+        //text field for element description
+        $mform->addElement(
+            'editor',
+            'description',
+            get_string('description', 'local_ulcc_form_library'),
+            array('class' => 'form_input','rows'=> '10', 'cols'=>'65')
+        );
+
+        $mform->addRule('description', null, 'maxlength', 10000, 'client');
+        $mform->setType('description', PARAM_RAW);
+
+
+
+        $this->specific_definition($mform);
+
+        //add the submit and cancel buttons
+        $this->add_action_buttons(true, get_string('submit'));
+    }
+
     /**
      * @param MoodleQuickForm $mform
      */
