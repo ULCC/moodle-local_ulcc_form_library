ce169a6101aa07ce31a34a17656f09e7f8668849
Matt Gibson <matt@inaura.net>
2012-09-26 22:54:27 +0100

    Code checker

--- plugin/form_elements/form_element_plugin_grade_mform.php
+++ plugin/form_elements/form_element_plugin_grade_mform.php
@@ -1,14 +1,35 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
 
 require_once($CFG->dirroot.'/local/ulcc_form_library/classes/form_element_plugin_mform.class.php');
 
+/**
+ * Form to add a grade selector to the dynamic form.
+ */
 class form_element_plugin_grade_mform  extends form_element_plugin_mform {
 
-
-
+    /**
+     * @param MoodleQuickForm $mform
+     */
     protected function specific_definition(MoodleQuickForm $mform) {
-
-        global  $DB;
 
         $mform->addElement('html', '<div>'.get_string('form_element_plugin_grade_max100', 'local_ulcc_form_library').'</div>');
 
@@ -25,6 +46,10 @@ class form_element_plugin_grade_mform  extends form_element_plugin_mform {
 
     }
 
+    /**
+     * @param $data
+     * @return array
+     */
     protected function specific_validation($data) {
 
         $data = (object) $data;
@@ -36,9 +61,14 @@ class form_element_plugin_grade_mform  extends form_element_plugin_mform {
         return $this->errors;
     }
 
+    /**
+     * @param $data
+     * @return bool|mixed
+     */
     protected function specific_process_data($data) {
 
-        $plgrec = (!empty($data->formfield_id)) ? $this->dbc->get_form_element_record("ulcc_form_plg_grade", $data->formfield_id) : false;
+        $plgrec = (!empty($data->formfield_id)) ?
+            $this->dbc->get_form_element_record("ulcc_form_plg_grade", $data->formfield_id) : false;
 
         if (empty($plgrec)) {
             return $this->dbc->create_form_element_record("ulcc_form_plg_grade", $data);
@@ -54,11 +84,6 @@ class form_element_plugin_grade_mform  extends form_element_plugin_mform {
             return $this->dbc->update_form_element_record("ulcc_form_plg_grade", $pluginrecord);
         }
     }
-
-    function definition_after_data() {
-
-    }
-
 
 
 }
